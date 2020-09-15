d8ee5107952b917b1c94cdb65cbfab4a6c1751cd
Kasimir Jyry Santeri Ilmonen <jyrysanteri@hotmail.com>
2020-09-16 00:40:46 +0300

    Lisatty edellisiin siirtoihin palaaminen.

--- src/index.js
+++ src/index.js
@@ -11,54 +11,18 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-      this.state = {
-        squares: Array(9).fill(null),
-        xIsNext: true,
-      };
-  }
-
-  // Antaa X:n tai O:n sen mukaan kenen vuoro on
-  annaVuoro(){
-     return this.state.xIsNext ? 'X' : 'O';
-  }
-
-  // Käsitellään klikkaus
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    // Joku on jo voittanut tai ruudussa on joku merkki  
-    if (calculateWinner(squares) || squares[i]) {
-        return;
-    }
-    squares[i] = this.annaVuoro();
-    this.setState({
-        squares: squares,
-        xIsNext: !this.state.xIsNext
-    });
-  }
-
   renderSquare(i) {
     return (
       <Square 
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-        status = 'Voittaja: ' + winner;
-    } else {
-        status = 'Seuraava pelaaja: ' + this.annaVuoro();
-    }
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -80,15 +44,82 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  // Aluestetaan historia
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      stepNumber: 0,
+      xIsNext: true,
+    };
+  }
+
+  // Käsitellään klikkaus
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    // Joku on jo voittanut tai ruudussa on joku merkki  
+    if (calculateWinner(squares) || squares[i]) {
+        return;
+    }
+    squares[i] = this.annaVuoro();
+    this.setState({
+        history: history.concat([{
+        squares: squares,
+        }]),
+        stepNumber: history.length,
+        xIsNext: !this.state.xIsNext
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    });
+  }
+
+  // Antaa X:n tai O:n sen mukaan kenen vuoro on
+  annaVuoro(){
+     return this.state.xIsNext ? 'X' : 'O';
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Siirry vuoroon ' + move :
+        'Siirry pelin alkuun';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = 'Voittaja: ' + winner;
+    } else {
+      status = 'Seuraava pelaaja: ' + this.annaVuoro();
+    }
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board 
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
